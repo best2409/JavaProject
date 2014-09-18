3c01979d61c5e0cc2200ea793e8695eca89f540b
best2409 <best2409@naver.com>
2014-09-18 22:27:39 +0900

    369 게임 주석 추가!

--- BasicProject/src/loop/ThreeSixNineExam.java
+++ BasicProject/src/loop/ThreeSixNineExam.java
@@ -9,24 +9,28 @@ public class ThreeSixNineExam {
 		int num1 = 0;	// 1의 자리 저장 
 		
 		for(int i=1; i<=99; i++) {
-			num10 = i / 10;			// 10의 자리 
-			num1 = i % 10;			// 1의 자리 
-			int clap = 0;	// 박수 횟수
+			num10 = i / 10;			// 10의 자리 계산 
+			num1 = i % 10;			// 1의 자리 계산 
+			int clap = 0;	// 박수 횟수 저장 
 			
-						
+			
+			// 369 게임 규칙 조건 만족 여부 확인 
 			if(num10 == 3 || num10 == 6 || num10 == 9)
-				clap++;
+				clap++;			// 박수 횟수 증가 
 			
 			if(num1 == 3 || num1 == 6 || num1 == 9)
-				clap++;
+				clap++;			// 박수 횟수 증가 
 			
 			
+			// 해당 숫자의 순서 출력 
 			System.out.print("\n" + num10 + num1 + "=======>");
 			
+			// 박수 횟수에 따른 결과 출력 
 			if(clap == 0)
 				System.out.print("" + num10 + num1);
+			
 			else if(clap == 1)
-			System.out.print("짝");
+				System.out.print("짝");
 			
 			else if(clap == 2)
 				System.out.print("짝짝");
