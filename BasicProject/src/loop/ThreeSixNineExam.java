db9416ac9b782387b59a50f7b8f8cc157babbc5b
best2409 <best2409@naver.com>
2014-09-18 22:24:10 +0900

    369 게임!

--- BasicProject/src/loop/ThreeSixNineExam.java
+++ BasicProject/src/loop/ThreeSixNineExam.java
@@ -5,6 +5,33 @@ public class ThreeSixNineExam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int num10 = 0;		// 10의 자리 저장 
+		int num1 = 0;	// 1의 자리 저장 
+		
+		for(int i=1; i<=99; i++) {
+			num10 = i / 10;			// 10의 자리 
+			num1 = i % 10;			// 1의 자리 
+			int clap = 0;	// 박수 횟수
+			
+						
+			if(num10 == 3 || num10 == 6 || num10 == 9)
+				clap++;
+			
+			if(num1 == 3 || num1 == 6 || num1 == 9)
+				clap++;
+			
+			
+			System.out.print("\n" + num10 + num1 + "=======>");
+			
+			if(clap == 0)
+				System.out.print("" + num10 + num1);
+			else if(clap == 1)
+			System.out.print("짝");
+			
+			else if(clap == 2)
+				System.out.print("짝짝");
+			
+		}
 	}
 
 }
